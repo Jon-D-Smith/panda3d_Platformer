635566de64f089baff2c03518966399173a7da8a
Jon Smith <aardvarkjon94@yahoo.com>
2021-11-26 18:05:28 -0600

    Added models to the screen

--- platformer.py
+++ platformer.py
@@ -1,9 +1,23 @@
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import loadPrcFileData
+
+configVars = """
+win-size 1280 720
+show-frame-rate-meter 1
+"""
+
+loadPrcFileData("", configVars)
 
 class Platformer(ShowBase):
     def __init__(self):
         super().__init__()
         self.set_background_color(0,0,0,1)
+
+        self.player = self.loader.loadModel("egg-models/player")
+        self.player.reparentTo(self.render)
+
+        self.floor = self.loader.loadModel("egg-models/floor.egg")
+        self.floor.reparentTo(self.render)
 
 game = Platformer()
 game.run()
