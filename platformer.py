c9bfbffb17e575bc8fcafa1749ab4058ede61fa4
Jon Smith <aardvarkjon94@yahoo.com>
2021-11-27 11:05:55 -0600

    basic jump

--- platformer.py
+++ platformer.py
@@ -14,56 +14,64 @@ key_map = {
     "right": False,
 }
 
+
 def update_key_map(control_name, state):
     key_map[control_name] = state
     print(state)
 
+
 class Platformer(ShowBase):
     def __init__(self):
         super().__init__()
-        self.set_background_color(0,0,0,1)
-        self.cam.setPos(0,-65,15)
+        self.set_background_color(0, 0, 0, 1)
+        self.cam.setPos(0, -65, 15)
 
         self.player = self.loader.loadModel("egg-models/player")
-        self.player.find("**/Player").node().setIntoCollideMask(BitMask32.bit(2))
+        self.player.find(
+            "**/Player").node().setIntoCollideMask(BitMask32.bit(2))
         self.player.reparentTo(self.render)
 
         self.floor = self.loader.loadModel("egg-models/floor.egg")
         self.floor.reparentTo(self.render)
 
-        setup_point_light(self.render, (15,0,20))
-
+        setup_point_light(self.render, (15, 0, 20))
 
         self.accept("arrow_left", update_key_map, ["left", True])
         self.accept("arrow_left-up", update_key_map, ["left", False])
         self.accept("arrow_right", update_key_map, ["right", True])
         self.accept("arrow_right-up", update_key_map, ["right", False])
+        self.accept("arrow_up", self.jump)
 
         self.taskMgr.add(self.update, "update")
 
-        self.position = Vec3(0,0,15)
-        self.velocity = Vec3(0,0,0)
-        self.acceleration = Vec3(0,0,0)
+        self.position = Vec3(0, 0, 15)
+        self.velocity = Vec3(0, 0, 0)
+        self.acceleration = Vec3(0, 0, 0)
 
         self.SPEED = 4
         self.GRAVITY = -0.05
         self.JUMP_FORCE = 1.2
         self.FRICTION = -0.12
 
-
         self.cTrav = CollisionTraverser()
         self.queue = CollisionHandlerQueue()
         collider_node = CollisionNode("box-coll")
-        coll_box = CollisionBox((-1,-1,0), (1,1,4))
+        coll_box = CollisionBox((-1, -1, 0), (1, 1, 4))
         collider_node.setFromCollideMask(BitMask32.bit(1))
         collider_node.addSolid(coll_box)
         collider = self.player.attachNewNode(collider_node)
         self.cTrav.addCollider(collider, self.queue)
 
+        self.is_jumping = False
+
+    def jump(self):
+        self.is_jumping = True
+        self.velocity.z = self.JUMP_FORCE
+
     def update(self, task):
         dt = globalClock.getDt()
 
-        self.acceleration = Vec3(0,0, self.GRAVITY)
+        self.acceleration = Vec3(0, 0, self.GRAVITY)
 
         if key_map["right"]:
             self.acceleration.x = self.SPEED * dt
@@ -76,16 +84,20 @@ class Platformer(ShowBase):
 
         for entry in self.queue.getEntries():
             inp = entry.getIntoNodePath().getPos(self.render)
-            self.position.z = inp.z
+            if not self.is_jumping:
+                self.position.z = inp.z
+            else:
+                self.is_jumping = False
 
         if self.position.z <= -10:
             self.position.z = 30
             self.position.x = 0
-
+            self.velocity = Vec3(0, 0, 0)
 
         self.player.setPos(self.position)
 
         return task.cont
+
 
 game = Platformer()
 game.run()
